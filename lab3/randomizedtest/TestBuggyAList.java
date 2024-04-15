9eb8311c8f5b06df3bced2a4a1e6e2704ad8be79
parke45 <danparker2@gmail.com>
2024-04-15 07:50:26 -0700

    changes to second part of lab3

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,38 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+
+    @Test
+    public void testThreeAddThreeRemove() {
+
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        correct.addLast(4);
+        correct.addLast(5);
+        correct.addLast(6);
+
+        BuggyAList<Integer> incorrect = new BuggyAList<>();
+        incorrect.addLast(4);
+        incorrect.addLast(5);
+        incorrect.addLast(6);
+
+        assertEquals(correct.size(), incorrect.size());
+
+        assertEquals(correct.removeLast(), incorrect.removeLast());
+        assertEquals(correct.removeLast(), incorrect.removeLast());
+        assertEquals(correct.removeLast(), incorrect.removeLast());
+
+
+//        int B = L.removeLast();
+//
+//        assertTrue(A==B);
+
+    }
+
+//    public void testSquarePrimesSimpleZ() {
+//        IntList lst = IntList.of(5,5);
+//        boolean changed = IntListExercises.squarePrimes(lst);
+//        assertEquals("25 -> 25", lst.toString());
+//        assertTrue(changed==true);
+//    }
 }
+
