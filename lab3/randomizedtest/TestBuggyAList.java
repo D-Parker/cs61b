0fe08b8a405aab44dad07b5ff6146be82f91369f
parke45 <danparker2@gmail.com>
2024-04-16 03:00:34 -0700

    second part of Lab 3, submitted to grader

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -28,19 +28,57 @@ public class TestBuggyAList {
         assertEquals(correct.removeLast(), incorrect.removeLast());
         assertEquals(correct.removeLast(), incorrect.removeLast());
         assertEquals(correct.removeLast(), incorrect.removeLast());
+    }
 
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
 
-//        int B = L.removeLast();
-//
-//        assertTrue(A==B);
+        BuggyAList<Integer> K = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                K.addLast(randVal);
+
+//                System.out.println("addLast(" + randVal + ")");
+                assertEquals(L.size(), K.size());
+
+            } else if (operationNumber == 1) {
+                // size
+                int sizeL = L.size();
+                int sizeK = K.size();
+//                System.out.println("size L: " + sizeL + " size K: " + sizeK);
+                assertEquals(sizeL, sizeK);
+            }
+            else if (operationNumber == 2 && L.size() > 0 && K.size() > 0) {
+
+                int a = L.getLast();
+                int b = K.getLast();
+
+//                System.out.println("getLast(" + a + " " + b);
+                assertEquals(a,b);
+//                System.out.println("size check L " + L.size() + " K " + K.size());
+                assertEquals(L.size(),K.size());
+            }
+            else if (operationNumber == 3 && L.size() > 0 && K.size() > 0) {
+                int a = L.removeLast();
+                int b = K.removeLast();
+
+//                System.out.println("removeLast(" + a + " " + b);
+                assertEquals(a,b);
+//                System.out.println("size check L " + L.size() + " K " + K.size());
+                assertEquals(L.size(),K.size());
+            }
+        }
+
 
     }
 
-//    public void testSquarePrimesSimpleZ() {
-//        IntList lst = IntList.of(5,5);
-//        boolean changed = IntListExercises.squarePrimes(lst);
-//        assertEquals("25 -> 25", lst.toString());
-//        assertTrue(changed==true);
-//    }
+
 }
 
