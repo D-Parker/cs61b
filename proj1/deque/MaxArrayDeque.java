0aa7e4fabee406ebbbedfab8ed587323015fc692
parke45 <danparker2@gmail.com>
2024-06-07 18:25:06 -0700

    change maxarraydeque

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,16 +1,42 @@
-//package deque;
-//
-//public class MaxArrayDeque<T> extends ArrayDeque<T>{
-//
-//
-//    public MaxArrayDeque(Comparator<T> c) {
-//
-//    }
-//
-//    public T max()
-//
-//
-//    public T max(Comparator<T> c)
-//
-//
-//}
+package deque;
+
+import java.util.Iterator;
+
+import java.lang.Math;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+
+    private Comparator<T> comparator;
+
+    public MaxArrayDeque(Comparator<T> c) {
+
+        super();
+        comparator = c;
+    }
+
+
+    public T max() {
+        return max(comparator);
+    }
+
+    public T max(Comparator<T> c) {
+        if (this.isEmpty()) {
+            return null;
+        }
+
+        int maxValueIndex = 0;
+        for (int i = 1; i < size(); i++) {
+            if (c.compare(get(maxValueIndex), get(i)) < 0) {
+                maxValueIndex = i;
+            }
+        }
+
+        return get(maxValueIndex);
+
+    }
+
+
+}
+
