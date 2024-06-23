8b8e3e207e4387f88c78c7e3756ae7d495dbbd00
parke45 <danparker2@gmail.com>
2024-06-23 03:33:05 -0700

    added the printInOrder method

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -22,6 +22,21 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K,V> {
         }
     }
 
+    public void printInOrder() {
+        printInOrder(root);
+    }
+
+    private void printInOrder(BSTNode node) {
+        if (node == null) {
+            return;
+        }
+
+        printInOrder(node.left);
+        System.out.println(node.key.toString() + " -> " + node.value.toString());
+        printInOrder(node.right);
+    }
+
+
 public BSTMap() {
 //        this.root = root;
 
